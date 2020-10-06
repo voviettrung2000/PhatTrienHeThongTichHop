c0a37332f8c73ee3272a30dd548a73c8cc7386bb
voviettrung2000 <72064639+voviettrung2000@users.noreply.github.com>
2020-10-06 13:43:33 +0700

    Add files via upload

---
+++ bai2.java
@@ -0,0 +1,24 @@
+package bai2;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+public class bai2 {
+
+	
+	public static void main(String[] args)
+	{
+	try{
+	InetAddress host = InetAddress.getByName(null);
+	String hostName = host.getHostName();
+	System.out.println("Host name:"+hostName);
+	System.out.println("Dia chi IP:"+host.getHostAddress());
+	}
+	catch(UnknownHostException e)
+	{
+	System.out.println("Khong tim thay dia chi");
+
+	
+	}
+	}
+
+}
